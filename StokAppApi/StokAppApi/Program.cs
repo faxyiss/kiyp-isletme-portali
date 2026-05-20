ceed1af7135dcb79067986bc56722f8d7c718b6b
cagan ipek <cagygt@gmail.com>
2026-05-21 00:24:53 +0300

    feat: MySQL container ve otomatik tablo olusturma eklendi

--- StokAppApi/StokAppApi/Program.cs
+++ StokAppApi/StokAppApi/Program.cs
@@ -74,6 +74,13 @@ namespace StokAppApi
 
 			var app = builder.Build();
 
+			// Uygulama açılırken DB tablolarını otomatik oluştur
+			using (var scope = app.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+				db.Database.EnsureCreated();
+			}
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
